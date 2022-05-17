956c4fdb58141a9e687dd507e2aa222e78c529af
eos0103 <sola2014@naver.com>
2022-05-17 16:27:54 +0900

    NodeJS 기능

---
+++ assi.js
@@ -0,0 +1,6 @@
+const add = 'Hi';
+const min = 'By';
+
+exports.add = add;
+exports.min = min;
+exports.min1 = min;
---
+++ main.js
@@ -0,0 +1,7 @@
+const value = require('./assi');
+console.log(value)
+
+// const { min, add, min1 } = require('./assi');
+// console.log(min, add, min1);
+
+global.console.log("1")
