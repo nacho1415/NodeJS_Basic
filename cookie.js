4a38e7585535ea3893561b8c2800aca3797f2763
eos0103 <sola2014@naver.com>
2022-05-20 21:45:58 +0900

    express

--- cookie.js
+++ cookie.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Set-Cookie': 'mycookie=test'});
-    res.end('Hello Cookie');
+http.createServer((req, res) => {
+  console.log('이건가?',req.url, req.headers.cookie);
+//   console.log('dasd')
+  res.writeHead(200, { 'Set-Cookie': 'mycookie=test' });
+  res.end('Hello Cookie');
 })
-    .listen(8083, () => {
-        console.log('8083번 포트에서 서버 대기중입니다!');
-    });
+  .listen(8083, () => {
+    console.log('8083번 포트에서 서버 대기 중입니다!');
+  });
